0e799c5ed94512e15c814bca7cfa5275795e719f
gayan_madhusankha <madhusankhamrtit97@gmail.com>
2023-06-29 14:02:50 +0530

    last update

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-console.log(ulEl)
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click",function(){
-    //console.log("Button click from addEventListner")
-    myLeads.push(inputEl.value)
-    inputEl.value =""
-    renderLeads()
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    }) 
 })
 
-function renderLeads(){
+function render(leads) {
     let listItems = ""
-
-    for(let i=0; i<myLeads.length;i++){
-        //listItems += "<li><a href='"+myLeads[i]+"' target='_blank'>" + myLeads[i] + "</a></li>"
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a href='${myLeads[i]}' target='_blank'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
-    
     ulEl.innerHTML = listItems
 }
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
